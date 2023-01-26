e34e83410392ba6b05ac79e8742e79ce128f706d
4212ganteng <azizmuslim051@gmail.com.com>
2023-01-26 14:46:08 +0700

    image uopload

--- index.js
+++ index.js
@@ -3,8 +3,34 @@ const app = express();
 const port = 5000;
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const multer = require("multer");
+
+// multer config
+const upload = multer.diskStorage({
+  destination: (req, file, cb) => {
+    // root file saved
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().getTime() + "-" + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype == "image/png" ||
+    file.mimetype == "image/jpg" ||
+    file.mimetype == "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
 app.use(bodyParser.json()); //type json
+
+app.use(multer({ storage: upload, fileFilter: fileFilter }).single("image")); //type nya singgle REQ.BODY('image')
 
 // route
 const productRoutes = require("./src/routes/product");
@@ -14,7 +40,7 @@ app.use("/api/v1/admin", productRoutes);
 const conectDB = () => {
   try {
     mongoose.connect(
-      "mongodb+srv://root:root@cluster0.c0wpdeg.mongodb.net/?retryWrites=true&w=majority"
+      "mongodb+srv://root:root@cluster0.c0wpdeg.mongodb.net/shopee?retryWrites=true&w=majority"
     );
 
     // server
--- src/controllers/product.js
+++ src/controllers/product.js
@@ -1,22 +1,33 @@
-exports.findProduct = (req, res, next) => {
-  res.json({
-    message: "Success get All Product",
-    data: {
-      id: 1,
-      name: "popok",
-      title: "charm",
-    },
-  });
+const Product = require("../models/Product");
+
+exports.findProduct = async (req, res, next) => {
+  try {
+    const data = await Product.find();
+
+    res.status(200).json({
+      status: "success",
+      data: data,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
-exports.createProduct = (req, res, next) => {
+exports.createProduct = async (req, res, next) => {
   console.log(req.body);
+  console.log(req.path, "pathhhh");
+  // const field = ({ title, price, name } = req.body);
+  try {
+    const data = await Product.create({
+      ...req.body,
+      image: req.file.path,
+    });
 
-  const data = ({ name, title, price, img } = req.body);
-  res.json({
-    message: "Success create Product",
-    data: {
-      data,
-    },
-  });
+    res.status(200).json({
+      status: "success",
+      data: data,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
 };
