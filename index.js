806ba726491c1b3ec48c6191ffdd98eadd814b08
4212ganteng <azizmuslim051@gmail.com.com>
2023-01-26 10:48:53 +0700

    post

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json()); //type json
 
 // route
 const productRoutes = require("./src/routes/product");
-app.use("/", productRoutes);
+app.use("/api/v1/admin", productRoutes);
 
 // server
 app.listen(port, () => {
